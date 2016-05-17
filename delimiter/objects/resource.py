4b9a5cd07d454b1d3fe003f4393763a9de45f1d0
Vilobh Meshram <vilobhmm@yahoo-inc.com>
2016-05-17 11:58:45 -0700

    Add Resource and Usage objects
    Change-Id: Ia8517fc36954f1bb982d532d98aed41a2b132eae

---
+++ delimiter/objects/resource.py
@@ -0,0 +1,33 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Resource(object):
+    """
+    Represent a resource.
+    """
+
+    def __init__(self, service, name, params=None):
+        """
+        Initialize a Resource.
+        :param service: The Service object the resource is associated
+                        with.
+        :param name: The name of the resource, as a string; e.g.,
+                     "instances", "vcpus", "images", etc.
+        :param params: The names of the parameters necessary to
+                       identify the resource.
+        """
+
+        self.service = service
+        self.name = name
+        self.params = set(params) if params else set()
